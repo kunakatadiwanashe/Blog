42a6ddb0ea8cea982f9adf9293d9be1e3cec4c91
kunakatadiwanashe <77622534+kunakatadiwanashe@users.noreply.github.com>
2021-11-19 18:31:38 +0200

    modelblogs

--- routes/blogs.js
+++ routes/blogs.js
@@ -1,6 +1,7 @@
 var express = require('express')
 var router = express.Router();
-const Blog = ('../models/blog.js');
+// const Blog = ('../models/blog.js');
+ const Blog = require('../models/blog')
 
 router.get('/', (req, res, next) => {
     // res.render('blogs')
